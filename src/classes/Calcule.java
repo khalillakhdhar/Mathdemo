0807ec986512cfdc71963914ae48eabcfdb0f81a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-17 16:17:29 +0100

    codes

--- src/classes/Calcule.java
+++ src/classes/Calcule.java
@@ -3,4 +3,36 @@ package classes;
 public class Calcule {
 private int n;
 
+public int getN() {
+	return n;
+}
+
+public void setN(int n) {
+	this.n = n;
+}
+
+
+public boolean parite()
+{
+	return this.n %2 ==0;
+	
+}
+public String  testepaire()
+
+{
+if(parite())
+	return "paire";
+else return "impaire";
+
+}
+double factoriel()
+{
+double f=1;
+for (int i=2;i<=this.n;i++)
+{
+f*=i;	
+}
+
+return f;
+}
 }
--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -1,10 +1,13 @@
 package testes;
 
+import classes.Calcule;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Calcule c=new Calcule();
+c.setN(4);
 	}
 
 }
